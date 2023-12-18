0b0ecd0c010523629178730c2a85db28c990134b
harikumarst <hari.subramani@talabat.com>
2023-12-18 22:20:25 +0000

    inserted more random strings

--- qa/main.tf
+++ qa/main.tf
@@ -8,12 +8,34 @@ terraform {
   }
 }
 
-resource "random_string" "random_qa" {
+resource "random_string" "random_qa_1" {
   length           = 16
   special          = true
   override_special = "/@£$"
 }
 
-output "random_string_qa_output_demo" {
-    value = random_string.random_qa.result
+resource "random_string" "random_qa_2" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
+
+resource "random_string" "random_qa_3" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
+
+output "random_string_qa_output_demo_1" {
+    value = random_string.random_qa_1.result
+}
+
+
+output "random_string_qa_output_demo_2" {
+    value = random_string.random_qa_2.result
+}
+
+
+output "random_string_qa_output_demo_3" {
+    value = random_string.random_qa_3.result
 }
